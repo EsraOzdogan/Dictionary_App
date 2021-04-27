31d3b8bf71ba76a60ac828d512b31940a0653de1
EsraOzdogan <esraozdogaan@gmail.com>
2021-04-28 01:17:05 +0300

    Add project files.

---
+++ DataAccessLayer/Migrations/202104272209483_mig1.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Contents", "WriteID", c => c.Int());
+            CreateIndex("dbo.Contents", "WriteID");
+            AddForeignKey("dbo.Contents", "WriteID", "dbo.Writers", "WriteID");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Contents", "WriteID", "dbo.Writers");
+            DropIndex("dbo.Contents", new[] { "WriteID" });
+            DropColumn("dbo.Contents", "WriteID");
+        }
+    }
+}
---
+++ MvcCamp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcCamp.MvcApplication" Language="C#" %>
