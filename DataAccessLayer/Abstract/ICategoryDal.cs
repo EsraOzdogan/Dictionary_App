bc6632685c14ebb76d7f0c1793b44ab7703d0d82
EsraOzdogan <esraozdogaan@gmail.com>
2021-04-28 02:40:36 +0300

    Refactoring

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    interface ICategoryDal: IRepository<Category>
+    {
+        //CRUD
+        //Type Name();
+        //List<Category> List();
+        //void Insert(Category p);
+        //void Update(Category p);
+        //void Delete(Category p);
+
+    }
+}
